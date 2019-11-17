2673e75d822b83936162745596e168c57d2d1057
Rafael <flns_rafa@hotmail.com>
2019-11-17 20:08:16 +0000

    mensagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formulario';
+
+  cliente = new Cliente();
+  profissoes = ['Programador', 'Empresário', 'Outra'];
+  profissao = 'Outra';
+
+  salvar(form: NgForm) {
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+    console.log(form);
+    form.reset({nome: 'Sebastião', profissao: ''}); // pode colocar valores default se quiser
+    // console.log(this.cliente);
+  }
 }
